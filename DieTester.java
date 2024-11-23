6b09ce8bcb3181c2dfdb8bc3f9d0d25859f45519
Aaron <127899108+WAAJ123@users.noreply.github.com>
2024-11-22 16:50:48 -0800

    Die and DieTester Classes
    Die class is prewritten by professor, need to complete DieTester.

---
+++ DieTester.java
@@ -0,0 +1,15 @@
+/* 
+    Aaron Johnson
+    CS 142 Week 8
+    11/22/2024
+    This program creates a Die object and tests all of the methods in the Die class
+*/
+
+public class DieTester 
+{
+    public static void main(String[] args) 
+    {
+        Die dieTest1 = new Die();
+        Die dieTest2 = new Die();
+    }
+}
